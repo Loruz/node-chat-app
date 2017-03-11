a3fffd6620eb95522c8014b7d4fd3400b15ecb5b
Vytautas <vpreiks@gmail.com>
2017-03-11 14:18:14 +0200

    Emit newMessage on createMEssage

--- server/server.js
+++ server/server.js
@@ -27,14 +27,15 @@ io.on('connection', (socket)=> {
         console.log('createEmail', newEmail);
 	})
 
-	socket.emit('newMessage', {
-		from:'user@example.com',
-		text: 'Zinute',
-		createdAt: 1212
-	})
 
 	socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+        	from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
+
 	})
 
     socket.on('disconnect', () => {
