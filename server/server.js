6c447cedd23e0f7c21e407c61d73f8db95cfe855
Vytautas <vpreiks@gmail.com>
2017-03-11 14:13:54 +0200

    Add new message and create message event

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 
 let app = express()
-let server = http.createServer(app);
+let server = http.createServer(app)
 let io = socketIO(server)
 
 
@@ -16,10 +16,32 @@ app.use(express.static(publicPath))
 
 io.on('connection', (socket)=> {
     console.log('New user connected');
+
+    socket.emit('newEmail', {
+    	from: 'mike@example.com',
+		text: 'Hei what up',
+		createdAt: 123
+	})
+
+	socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+	})
+
+	socket.emit('newMessage', {
+		from:'user@example.com',
+		text: 'Zinute',
+		createdAt: 1212
+	})
+
+	socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+	})
+
     socket.on('disconnect', () => {
         console.log('User has disconnected');
     })
 })
+
 
 
 
